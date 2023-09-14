215a8a474bded919a1666cf883582cad6c3a065f
Vitthal Jadhav <jadhav.vitthal25>
2023-09-14 21:40:32 +0530

    adding some message

---
+++ add/src/add/Operation.java
@@ -0,0 +1,12 @@
+package add;
+
+public class Operation {
+
+	public static void main(String[] args) {
+
+		System.out.println("Performing addition operation");
+		System.out.println(10+20);
+		System.out.println("Hello , I am Developer so i am developing web application ");
+	}
+
+}
